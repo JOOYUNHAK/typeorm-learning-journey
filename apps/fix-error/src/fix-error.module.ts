5ebb47dd19b3b6f6b1d84dcc3e7c112a179ffa0f
JOOYUNHAK <wndbsgkr@naver.com>
2025-04-29 19:44:53 +0900

    add: typeorm 모듈 설정

--- apps/fix-error/src/fix-error.module.ts
+++ apps/fix-error/src/fix-error.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { FixErrorController } from './fix-error.controller';
-import { FixErrorService } from './fix-error.service';
+import { Module } from "@nestjs/common";
+import { FixErrorController } from "./fix-error.controller";
+import { FixErrorService } from "./fix-error.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "test",
+      database: "typeorm",
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [FixErrorController],
   providers: [FixErrorService],
 })
